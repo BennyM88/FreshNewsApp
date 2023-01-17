4d82479b913411cb49074a7c9d47424c38449ac4
Patryk <patryk@kotrem.pl>
2023-01-17 14:39:19 +0100

    Added signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,24 +1,31 @@
 const User = require('../models/user');
+const config = require('../config/config');
+const jwt = require('jsonwebtoken');
 
 // handle errors
 const handleErrors = (e) => {
     let errors = { email: '', password: '' };
 
-    // duplicate email error
     if (e.code === 11000) {
         errors.email = 'That email already exists';
         return errors;
     }
 
-    // validation errors
     if (e.message.includes('user validation failed')) {
         Object.values(e.errors).forEach(({ properties }) => {
             errors[properties.path] = properties.message;
         });
     }
-
     return errors;
 }
+
+// create json web token
+const maxAge = 2 * 24 * 60 * 60;
+const createToken = (id) => {
+    return jwt.sign({ id }, config.jwtKey, {
+        expiresIn: maxAge
+    });
+};
 
 module.exports.signup_get = async (ctx) => {
     await ctx.render('signup');
@@ -33,11 +40,15 @@ module.exports.signup_post = async (ctx) => {
 
     try {
         const user = await User.create({ email, password });
-        ctx.status(201).json(user);
+        const token = createToken(user._id);
+        ctx.cookies.set('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+        ctx.status = 201;
+        ctx.body = { id: user._id };
     }
     catch (e) {
         const errors = handleErrors(e);
-        ctx.status(400).json({ errors });
+        ctx.status = 400;
+        ctx.body = errors;
     }
 }
 
